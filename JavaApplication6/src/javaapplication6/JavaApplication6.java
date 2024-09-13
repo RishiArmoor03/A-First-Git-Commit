9caf0335c5832b8f3840dfc1f27625f19c7087cb
RishiArmoor03 <144120735+RishiArmoor03@users.noreply.github.com>
2024-09-12 19:50:33 -0500

    This is my First Git Summit Assignment

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -11,7 +11,7 @@ package javaapplication6;
 public class JavaApplication6 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world"); //Prints hello world.
     }
     
 }
